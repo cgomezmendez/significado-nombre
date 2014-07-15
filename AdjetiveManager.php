606213c20968a5690676e51b2da55021964160aa
cgomezmendez <contact@cristiangomez.me>
2014-07-15 04:05:35 -0400

    Added class to manage adjective

---
+++ AdjetiveManager.php
@@ -0,0 +1,21 @@
+<?php
+class AdjetiveManager{
+	private $adjectives;
+	public function __construct () {
+		$jsonadjectives = file_get_contents('adjetivos.json');
+		$this->adjectives = json_decode($jsonadjectives);
+	}
+	public function getAdjectivetartWith($char) {
+		$adjectivesW = array();
+		foreach ($this->adjectives as $key => $adjective) {
+			if (strtoupper($adjective[0])===strtoupper($char)){
+				array_push($adjectivesW, $adjective);
+			}
+		}
+		$key = rand(0,count($adjectivesW)-1);
+		$adjective = $adjectivesW[$key];
+		$key = array_search($adjective,$this->adjectives);
+		unset($this->adjectives[$key]);
+		return $adjective;
+	}
+}
